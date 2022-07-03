1040fdd6930ef45773bd1f900356f6bfb6def864
akshatk806 <106431065+akshatk806@users.noreply.github.com>
2022-07-04 00:22:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,196 @@
+// You have pretend to use a word api which will contain an object ,you have to take the input from the input tag we have to print the defination from all the result of that word API , you have to print in DOM
+// You have to print the defination in DOM which you will give in input tag
+
+const myJson=`{
+    "word": "example",
+    "results": [
+      {
+        "definition": "a representative form or pattern",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "model"
+        ],
+        "typeOf": [
+          "representation",
+          "internal representation",
+          "mental representation"
+        ],
+        "hasTypes": [
+          "prefiguration",
+          "archetype",
+          "epitome",
+          "guide",
+          "holotype",
+          "image",
+          "loadstar",
+          "lodestar",
+          "microcosm",
+          "original",
+          "paradigm",
+          "pilot",
+          "prototype",
+          "template",
+          "templet",
+          "type specimen"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "I profited from his example"
+        ]
+      },
+      {
+        "definition": "something to be imitated",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exemplar",
+          "good example",
+          "model"
+        ],
+        "typeOf": [
+          "ideal"
+        ],
+        "hasTypes": [
+          "pacemaker",
+          "pattern",
+          "beauty",
+          "prodigy",
+          "beaut",
+          "pacesetter"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ]
+      },
+      {
+        "definition": "an occurrence of something",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "case",
+          "instance"
+        ],
+        "typeOf": [
+          "happening",
+          "natural event",
+          "occurrence",
+          "occurrent"
+        ],
+        "hasTypes": [
+          "clip",
+          "mortification",
+          "piece",
+          "time",
+          "humiliation",
+          "bit"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "but there is always the famous example of the Smiths"
+        ]
+      },
+      {
+        "definition": "an item of information that is typical of a class or group",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "illustration",
+          "instance",
+          "representative"
+        ],
+        "typeOf": [
+          "information"
+        ],
+        "hasTypes": [
+          "excuse",
+          "apology",
+          "specimen",
+          "case in point",
+          "sample",
+          "exception",
+          "quintessence",
+          "precedent"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ],
+        "examples": [
+          "this patient provides a typical example of the syndrome",
+          "there is an example on page 10"
+        ]
+      },
+      {
+        "definition": "punishment intended as a warning to others",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "deterrent example",
+          "lesson",
+          "object lesson"
+        ],
+        "typeOf": [
+          "monition",
+          "admonition",
+          "word of advice",
+          "warning"
+        ],
+        "derivation": [
+          "exemplary"
+        ],
+        "examples": [
+          "they decided to make an example of him"
+        ]
+      },
+      {
+        "definition": "a task performed or problem solved in order to develop skill or understanding",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exercise"
+        ],
+        "typeOf": [
+          "lesson"
+        ],
+        "examples": [
+          "you must work the examples at the end of each chapter in the textbook"
+        ]
+      }
+    ],
+    "syllables": {
+      "count": 3,
+      "list": [
+        "ex",
+        "am",
+        "ple"
+      ]
+    },
+    "pronunciation": {
+      "all": "ɪɡ'zæmpəl"
+    },
+    "frequency": 4.67
+}`;                            // anything inside back tics consider as string 
+
+
+const myObj=JSON.parse(myJson);      // confirm that the JSON is valid JSON or not sometimes it is not valid
+console.log("The object is:",myObj);
+console.log("The results in myObj is:",myObj["results"]);   // to printing all the definations // Array of object
+
+
+let defination=document.getElementById('defination');
+defination.addEventListener('click',function(){
+    console.log("Someone clicks on defination");
+    populate();
+});
+
+
+function populate(){
+    let results=myObj['results'];
+    console.log("In populate function:",results);
+    let html="";
+    results.forEach(function(element){
+        html+=`<li>One of the definations of example is ${element.definition}</li>`;
+    });
+    let defs=document.getElementById('defs');
+    defs.innerHTML=html;
+}
